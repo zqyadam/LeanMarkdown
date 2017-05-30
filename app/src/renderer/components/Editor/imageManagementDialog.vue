70abc3dbe856fd2ff14d1c0e6440b2913c0a11dd
zqyadam <zqyadam@qq.com>
2017-05-30 13:52:46 +0800

    增加图片管理中的图片预览、添加日期显示

--- app/src/renderer/components/Editor/imageManagementDialog.vue
+++ app/src/renderer/components/Editor/imageManagementDialog.vue
@@ -2,18 +2,24 @@
   <el-dialog v-model="showDialog" :close-on-press-escape="true" :modal="false" :close-on-click-modal="false" size="full" @open="open" @close="close">
     <span slot="title"><i class="el-icon-z-iconfontcolor16"></i><span style="margin-left:5px;">图片管理</span></span>
     <el-table style="width: 100%;" :data="tableData" :border="true" empty-text="暂无分类数据" v-loading="loading" element-loading-text="拼命加载中" height="442">
-      <el-table-column label="图片URL" property="">
+      <el-table-column label="图片URL" property="imageURL">
         <template scope="scope">
-          <span v-text="scope.row.url()"></span>
-          <el-popover placement="top-start" title="标题" width="200" trigger="hover" content="这是一段内容,这是一段内容,这是一段内容,这是一段内容。">
+          <el-popover placement="top-start" trigger="hover">
+            <span v-text="scope.row.get('url')" slot="reference"></span>
+            <img :src="scope.row.get('url')" height="auto" width="150">
           </el-popover>
         </template>
       </el-table-column>
-      <el-table-column label="所属文章" property="attribute.referedPosts">
+      <el-table-column label="所属文章" property="attributes.metaData.referedPosts">
+      	<template scope="scope">
+      		<div>
+      			{{scope.row.attributes.metaData.referedPosts}}
+      		</div>
+      	</template>
       </el-table-column>
-      <el-table-column label="添加日期">
+      <el-table-column label="添加日期" :formatter="rendercreatedAtRow" width="170">
       </el-table-column>
-      <el-table-column label="操作">
+      <el-table-column label="操作" width="80">
         <template scope="scope">
           <el-button size="small" @click="deleteImage(scope.row)">删除</el-button>
         </template>
@@ -25,6 +31,8 @@
 import {
   getAllImages
 } from '../../js/api'
+
+import moment from 'moment/moment.js'
 
 export default {
   data() {
@@ -57,11 +65,15 @@ export default {
         // console.log(imagePromise);
         imagePromise.then((images) => {
           console.log(images);
+          this.filterdArr = images;
         })
       },
       close: function() {
 
-      }
+      },
+      rendercreatedAtRow: function(image) {
+        return moment(image.createdAt).format("YYYY-MM-DD  H:mm:ss");
+      },
     }
 }
 </script>
